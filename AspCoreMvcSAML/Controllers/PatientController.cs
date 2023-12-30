8fdad8fc37204ad7878172760e31fd815f62ea4e
hehaibo@hotmail.com <hehaibo@hotmail.com>
2023-12-30 10:49:56 -0500

    init

---
+++ AspCoreMvcSAML/Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using AspCoreMvcSAML.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Abstractions;
+using AspCoreMvcSAML.Models;
+
+namespace AspCoreMvcSAML.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IDownstreamApi _downstreamApi;
+        private readonly ILogger<HomeController> _logger;
+        public PatientController(ILogger<HomeController> logger, IDownstreamApi downstreamApi)
+        {
+            _logger = logger;
+            _downstreamApi = downstreamApi; ;
+        }
+        public ActionResult Patients(PatientViewModel pats)
+        {
+            pats = new PatientViewModel();
+            return View("Patients",pats);
+        }
+    }
+}
---
+++ AspCoreMvcSAML/Models/PatientModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCoreMvcSAML.Models
+{
+    public class PatientViewModel
+    {
+        public PatientViewModel()
+        {
+            Patients = new List<PatientModel>()
+            {
+
+                new PatientModel { PatientID = 100, FirstName = "John", LastName = "Smith", DOB = Convert.ToDateTime("2/1/2022") } ,
+                new PatientModel { PatientID = 200, FirstName = "Trinity", LastName = "Lin", DOB = Convert.ToDateTime("12/24/1987") },
+                new PatientModel { PatientID = 300, FirstName = "Muhammed", LastName = "Church", DOB = Convert.ToDateTime("5/4/1997") },
+                new PatientModel { PatientID = 400, FirstName = "Maisy", LastName = "George", DOB = Convert.ToDateTime("6/15/1954") }
+
+            };
+        }
+
+        public IEnumerable<PatientModel> Patients { get; set; }
+
+    }
+    public class PatientModel
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
